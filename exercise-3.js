ab9e986f95d9251881d19e2bb4f4286e05bbfd61
Jasmin <108983170+jasmincaroline@users.noreply.github.com>
2025-08-25 15:58:25 -0300

    Update exercise-3.js

--- exercise-3.js
+++ exercise-3.js
@@ -1,3 +1,7 @@
+//3) Crie um programa que lê o ano de nascimento de uma pessoa e o ano atual. Calcule e
+//mostre qual é: a idade da pessoa em anos, a idade da pessoa em meses, a idade da
+//pessoa em dias e a idade da pessoa em semanas.
+  
 let anoNascimento = prompt("Ano de nascimento:");
 let anoAtual = prompt("Ano atual:");
 
