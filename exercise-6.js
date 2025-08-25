a3d3939e272a7285ed0b5b1b2ca2c9790ecf0b89
Jasmin <108983170+jasmincaroline@users.noreply.github.com>
2025-08-25 16:17:13 -0300

    Update exercise-6.js

--- exercise-6.js
+++ exercise-6.js
@@ -1,5 +1,6 @@
 //6) Elaborar um programa que calcule e apresente o valor do volume de uma caixa
 //retangular, utilizando a fórmula VOLUME <- COMPRIMENTO * LARGURA * ALTURA.
+
 let comprimento = parseFloat(prompt("Comprimento:"));
 let largura = parseFloat(prompt("Largura:"));
 let altura = parseFloat(prompt("Altura:"));
